e960ea807780cb9d237215b80e3f6c73932c3a1c
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-22 16:20:49 +0200

    fine 5o

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -1,4 +1,10 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
+const evenNumbers = nums.filter((even) => {
+  return even % 2 === 0;
+});
+
+console.log(evenNumbers);
+
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
