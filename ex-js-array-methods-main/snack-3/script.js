5977f8e5d77c2d0fa6a284acc226f602787cf2c5
MorettiAndrea <andrea.moretti1704@gmail.com>
2025-04-22 16:09:34 +0200

    fine 3o

--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -1,4 +1,8 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const numberPlusOner = numbers.map((numbers) => numbers + 1);
+
+console.log(numberPlusOner);
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
